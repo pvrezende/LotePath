ba5d1b3510ad2392f7b75e2d0f5fbfbc4c560f54
Paulo Rezende <paulo.rezende@deltasollutions.com.br>
2026-04-09 18:57:25 -0400

    feat: adiciona tratamento global de erro http e sessão

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -8,12 +8,13 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
   ],
 };
